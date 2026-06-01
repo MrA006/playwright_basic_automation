f1e2c4ad1b6f37e7528ed42642fd01a81fc3ab43
tech752 <tech@steelmarketing.org>
2026-06-01 16:55:49 +0500

    Add Allure reporting

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,5 +1,5 @@
 
-
+import { test } from '@playwright/test';
 
 class LoginPage {
     constructor(page){
@@ -17,12 +17,22 @@ class LoginPage {
 
     async goToUrl(){
         await this.page.goto ( 'https://www.adactinhotelapp.com/');
+        await this.attachScreenshot('Step 01 - navigate to login page');
     }
 
     async login(username, password){
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
+        await this.attachScreenshot('Step 02 - submit login credentials');
+    }
+
+    async attachScreenshot(){
+
+        await test.info().attach('screenshot',{
+            body: await this.page.screenshot(),
+            contentType: 'image/png'
+        })
     }
 
 
--- tests/loginPOM.spec.js
+++ tests/loginPOM.spec.js
@@ -5,18 +5,31 @@ import users from '../testData/loginData.json';
 let i = 0;
 
 
-users.data.forEach(user => {
+// users.data.forEach(user => {
 
-    test(`login test functionality for ${user.username} + ${i++}`, async ({page}) => {
+
+    test.describe(`Login test for ${users.data[0].username}`, () => {
+
+    test(`login test functionality for ${users.data[0].username}`, async ({page}) => {
 
         const loginPage = new LoginPage(page);
 
-        await loginPage.goToUrl();
+        await test.step('Navigate to login page and submit credentials', async () => {
+            await loginPage.goToUrl();
+        });
 
-        await loginPage.login(user.username, user.password);
+        await test.step('login using creds', async () => {
+
+            await loginPage.login(users.data[0].username, users.data[0].password);
+        })
+
+        await test.step('verify login successful', async () => {
 
         await expect(loginPage.welcomeMessage).toHaveText('Welcome to Adactin Group of Hotels');
+        });
 
     });
 
 });
+
+// });
